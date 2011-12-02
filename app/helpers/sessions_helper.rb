616a58ffb2a5bc2af46bea6a37f2bd3bb7ac47d7
Fang Chang <fkchang@gmail.com>
2011-12-01 23:53:16 -0800

    Done with user edit/update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,7 @@ module SessionsHelper
     # Session Only - no cookies
     #session[:remember_token] = user.id
     #self.current_user = user
+    #logger.debug "here i am!"
   end
   
   def current_user=(user)
@@ -17,6 +18,10 @@ module SessionsHelper
     @current_user ||= user_from_remember_token
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
   def signed_in?
     !current_user.nil?
   end
@@ -24,6 +29,16 @@ module SessionsHelper
   def sign_out
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+  
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
   end
 
   private
@@ -37,5 +52,13 @@ module SessionsHelper
 
     def remember_token
      cookies.signed[:remember_token] || [nil, nil]
+    end
+    
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
     end
 end
